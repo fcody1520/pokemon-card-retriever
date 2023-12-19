8584dde018ededa13bf58fd67b0ed5da644a5c31
Cody Farrington <fcody1520@gmail.com>
2023-12-19 16:12:34 -0700

    add event.target.value in new functions

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ const theList = ["pikachu","raichu", "voltorb"]
 function App() {
 
   const [pokemonList, setPokemonList] = useState(theList)
+  const [limit, setLimit] = useState('6')
+  const [type, setType] = useState('electric')
 
   function handleClick(){
     setPokemonList([...pokemonList, 'MewTwo'])
@@ -16,13 +18,27 @@ function App() {
 
   }
 
+  function handleLimit(evt){
+    setLimit(evt.target.value)
+  }
 
+  function handleType(evt){
+    setType(evt.target.value)
+  }
 
 
   return (
     <>
       <div>
         <h1>Here are some pokemon!</h1>
+        <form>
+          <label htmlFor='limit-input'>limit</label>
+          <input type="text" id='limit-input' value={limit} onChange={handleLimit}/>
+          <label htmlFor="type-input">Type</label>
+          <input type="text" id='type-input' value={type} onChange={handleType}/>
+          <button>Submit</button>
+        </form>
+
         {
           pokemonList.map((ele, idx, arr) => {
             return <Card pokemon={ele}/>
